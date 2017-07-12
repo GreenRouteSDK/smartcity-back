cf04f6f185b60892b9c80db5051703059416d8b6
Rachel <y.rachel.bg@gmail.com>
2017-07-12 11:25:11 -0500

    Se agregegaron saltos de línea en el campo descripción del catálogo de alertas. Se modificó el archivo notification_fila para agregar nuevas subalertas

--- src/main/java/mx/infotec/smartcity/backend/utils/AlertCatalog.java
+++ src/main/java/mx/infotec/smartcity/backend/utils/AlertCatalog.java
@@ -18,31 +18,31 @@ public class AlertCatalog {
         Float temperature = Float.parseFloat(valor);
         
         if(temperature<=-40){
-            return ("Temperature: " + valor + " °C Hyper-glacial: thermal very cold and extreme cold stress");
+            return ("Temperature: " + valor + " °C <br> Hyper-glacial: thermal very cold and extreme cold stress.");
         }
         else if(temperature>=-39&&temperature<=-20){
-            return ("Temperature: " + valor + " °C \nGlacial: thermal sensitivity cold and strong cold stress");
+            return ("Temperature: " + valor + " °C <br> Glacial: thermal sensitivity cold and strong cold stress.");
         }
         else if(temperature>=-19&&temperature<=-10){
-            return ("Temperature: " + valor + " °C \nExtremely cold: thermal sensitivity cool and moderate cold stress");
+            return ("Temperature: " + valor + " °C <br> Extremely cold: thermal sensitivity cool and moderate cold stress.");
         }
         else if(temperature>=-9&&temperature<=-1){
-            return ("Temperature: " + valor + " °C \nVery cold: thermal sensitivity slightly cool and slight cold stress.");
+            return ("Temperature: " + valor + " °C <br> Very cold: thermal sensitivity slightly cool and slight cold stress.");
         }
         else if(temperature>=0&&temperature<=13){
-            return ("Temperature: " + valor + " °C \nCold: thermal sensitivity comfortable and no thermal stress.");
+            return ("Temperature: " + valor + " °C <br> Cold: thermal sensitivity comfortable and no thermal stress.");
         }
         else if(temperature>=27&&temperature<=32){
-            return ("Temperature: " + valor + " °C \nCaution: Fatigue is possible with prolonged exposure and activity. Continuing activity could result in heat cramps.");
+            return ("Temperature: " + valor + " °C <br> Caution: Fatigue is possible with prolonged exposure and activity. Continuing activity could result in heat cramps.");
         }
         else if(temperature>=33&&temperature<=41){
-            return ("Temperature: " + valor + " °C \nAlert: Heat cramps and heat exhaustion are possible. Continuing activity could result in heat stroke.");
+            return ("Temperature: " + valor + " °C <br> Alert: Heat cramps and heat exhaustion are possible. Continuing activity could result in heat stroke.");
         }
         else if(temperature>=42&&temperature<=54){
-            return ("Temperature: " + valor + " °C \nWarning: Heat cramps and heat exhaustion are likely; heat stroke is probable with continued activity.");
+            return ("Temperature: " + valor + " °C <br> Warning: Heat cramps and heat exhaustion are likely; heat stroke is probable with continued activity.");
         }
         else if(temperature>54){
-            return("Temperature: " + valor + " °C \nDanger: Extreme danger: heat stroke is imminent.");
+            return("Temperature: " + valor + " °C <br> Danger: Extreme danger: heat stroke is imminent.");
         }
           return ("Temperature: " + valor + " °C");  
     }
@@ -65,15 +65,15 @@ public class AlertCatalog {
     public static String setAlertHumidity(String valor){
         Float humidity = Float.parseFloat(valor);
         if(humidity<=29){
-            return("Relative humidity: "+ valor + ". No discomfort");
+            return("Relative humidity: "+ valor + " %. <br> No discomfort.");
         }else if(humidity>=30&&humidity<=39){
-            return("Relative humidity: "+ valor + ". Some discomfort");
+            return("Relative humidity: "+ valor + " %. <br> Some discomfort.");
         }else if(humidity>=40&&humidity<=45){
-            return("Relative humidity: "+ valor + ". Great discomfort: avoid exertion");
+            return("Relative humidity: "+ valor + " %. <br> Great discomfort: avoid exertion.");
         }else if(humidity>=46&&humidity<=54){
-            return("Relative humidity: "+ valor + ". Dangerous");
+            return("Relative humidity: "+ valor + " %. <br> Dangerous.");
         }else if(humidity>54){
-            return("Relative humidity: "+ valor + ". Heat stroke imminent");
+            return("Relative humidity: "+ valor + " %. <br> Heat stroke imminent.");
         }
         
         return ("Relative humidity: " + valor);
@@ -85,66 +85,66 @@ public class AlertCatalog {
         switch(pollutant){
             case "PM10":
                 if(pollution>=0&&pollution<=50){
-                    return ("Particulate matter: " + valor + " \nGood: Air quality is considered satisfactory, and air pollution poses little or no risk.");
+                    return ("Particulate matter: " + valor + " GQ <br> Good: Air quality is considered satisfactory, and air pollution poses little or no risk.");
                 }else if(pollution>=51&&pollution<=100){
-                    return ("Particulate matter: " + valor + " \nAcceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
+                    return ("Particulate matter: " + valor + " GQ <br> Acceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
                 }else if(pollution>=101&&pollution<=250){
-                    return ("Particulate matter: " + valor + " \nModerately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
+                    return ("Particulate matter: " + valor + " GQ <br> Moderately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
                 }else if(pollution>=251&&pollution<=350){
-                    return ("Particulate matter: " + valor + " \nDamage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
+                    return ("Particulate matter: " + valor + " GQ <br> Damage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
                 }else if(pollution>=351&&pollution<=430)
-                    return ("Particulate matter: " + valor + " \nVery danger: It represents an emergency condition. The entire population is likely to be affected.");
+                    return ("Particulate matter: " + valor + " GQ <br> Very danger: It represents an emergency condition. The entire population is likely to be affected.");
                 break;
             case "NO2":
                 if(pollution>=0&&pollution<=40){
-                    return ("Nitrogen dioxide: " + valor + " \nGood: Air quality is considered satisfactory, and air pollution poses little or no risk.");
+                    return ("Nitrogen dioxide: " + valor + " PPB <br> Good: Air quality is considered satisfactory, and air pollution poses little or no risk.");
                 }else if(pollution>=41&&pollution<=80){
-                    return ("Nitrogen dioxide: " + valor + " \nAcceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
+                    return ("Nitrogen dioxide: " + valor + " PPB <br> Acceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
                 }else if(pollution>=81&&pollution<=180){
-                    return ("Nitrogen dioxide: " + valor + " \nModerately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
+                    return ("Nitrogen dioxide: " + valor + " PPB <br> Moderately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
                 }else if(pollution>=181&&pollution<=280){
-                    return ("Nitrogen dioxide: " + valor + " \nDamage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
+                    return ("Nitrogen dioxide: " + valor + " PPB <br> Damage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
                 }else if(pollution>=281&&pollution<=400){
-                    return ("Nitrogen dioxide: " + valor + " \nVery danger: It represents an emergency condition. The entire population is likely to be affected.");
+                    return ("Nitrogen dioxide: " + valor + " PPB <br> Very danger: It represents an emergency condition. The entire population is likely to be affected.");
                 }
                 break;
             case "O3":
                 if(pollution>=0&&pollution<=50){
-                    return ("Ozone: " + valor + " \nGood: Air quality is considered satisfactory, and air pollution poses little or no risk.");
+                    return ("Ozone: " + valor + " PPB <br> Good: Air quality is considered satisfactory, and air pollution poses little or no risk.");
                 }else if(pollution>=51&&pollution<=100){
-                    return ("Ozone: " + valor + " \nAcceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
+                    return ("Ozone: " + valor + " PPB <br> Acceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
                 }else if(pollution>=101&&pollution<=168){
-                    return ("Ozone: " + valor + " \nModerately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
+                    return ("Ozone: " + valor + " PPB <br> Moderately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
                 }else if(pollution>=169&&pollution<=208){
-                    return ("Ozone: " + valor + " \nDamage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
+                    return ("Ozone: " + valor + " PPB <br> Damage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
                 }else if(pollution>=209&&pollution<=748){
-                    return("Ozone: " + valor + " \nVery danger: It represents an emergency condition. The entire population is likely to be affected.");
+                    return("Ozone: " + valor + " PPB <br> Very danger: It represents an emergency condition. The entire population is likely to be affected.");
                 }
                 break;
             case "CO":
                 if(pollution>=0&&pollution<=40){
-                    return ("Carbon monoxide: " + valor + " \nGood: Air quality is considered satisfactory, and air pollution poses little or no risk.");
+                    return ("Carbon monoxide: " + valor + " PPM <br> Good: Air quality is considered satisfactory, and air pollution poses little or no risk.");
                 }else if(pollution>=41&&pollution<=80){
-                    return ("Carbon monoxide: " + valor + " \nAcceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
+                    return ("Carbon monoxide: " + valor + " PPM <br> Acceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
                 }else if(pollution>=81&&pollution<=380){
-                    return ("Carbon monoxide: " + valor + " \nModerately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
+                    return ("Carbon monoxide: " + valor + " PPM <br> Moderately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
                 }else if(pollution>=381&&pollution<=800){
-                    return ("Carbon monoxide: " + valor + " \nDamage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
+                    return ("Carbon monoxide: " + valor + " PPM <br> Damage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
                 }else if(pollution>=801&&pollution<=1600){
-                    return ("Carbon monoxide: " + valor + " \nVery danger: It represents an emergency condition. The entire population is likely to be affected.");
+                    return ("Carbon monoxide: " + valor + " PPM <br> Very danger: It represents an emergency condition. The entire population is likely to be affected.");
                 }
                 break;
             case "SO2":
                 if(pollution>=0&&pollution<=40){
-                    return ("Sulfur monoxide: " + valor + " \nGood: Air quality is considered satisfactory, and air pollution poses little or no risk.");
+                    return ("Sulfur monoxide: " + valor + " PPB <br> Good: Air quality is considered satisfactory, and air pollution poses little or no risk.");
                 }else if(pollution>=41&&pollution<=80){
-                    return ("Sulfur monoxide: " + valor + " \nAcceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
+                    return ("Sulfur monoxide: " + valor + " PPB <br> Acceptable: Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.");
                 }else if(pollution>=81&&pollution<=380){
-                    return ("Sulfur monoxide: " + valor + " \nModerately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
+                    return ("Sulfur monoxide: " + valor + " PPB <br> Moderately polluted: Children, older adults, people who engage in intense physical activity or with respiratory and cardiovascular diseases, should limit prolonged outdoor efforts.");
                 }else if(pollution>=381&&pollution<=800){
-                    return ("Sulfur monoxide: " + valor + " \nDamage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
+                    return ("Sulfur monoxide: " + valor + " PPB <br> Damage to health: Everyone can experience health effects; Who belong to sensitive groups may experience serious health effects.");
                 }else if(pollution>=801&&pollution<=1600){
-                    return("Sulfur monoxide: " + valor + " \nVery danger: It represents an emergency condition. The entire population is likely to be affected.");
+                    return("Sulfur monoxide: " + valor + " PPB <br> Very danger: It represents an emergency condition. The entire population is likely to be affected.");
                 }                
                 break;
         }
