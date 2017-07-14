7b8347c8d4639d3e7a08e86f01586cf78578b620
Rachel <y.rachel.bg@gmail.com>
2017-07-14 18:42:53 -0500

    Actualización del catálogo de alertas para dejar dos números después del punto

--- src/main/java/mx/infotec/smartcity/backend/utils/AlertCatalog.java
+++ src/main/java/mx/infotec/smartcity/backend/utils/AlertCatalog.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package mx.infotec.smartcity.backend.utils;
+import java.text.DecimalFormat;
 
 /**
  *
@@ -11,40 +12,43 @@ package mx.infotec.smartcity.backend.utils;
  */
 public class AlertCatalog {
     
+    static DecimalFormat decimalformat = new DecimalFormat("##.##");
+    
     public AlertCatalog(){
     }
     
     public static String setAlertTemperature(String valor){
         Float temperature = Float.parseFloat(valor);
+        String format = decimalformat.format(temperature);
         
         if(temperature<=-40){
-            return ("Temperature: " + valor + " °C <br> Classed as: Hyper-glacial. <br> Advice: Thermal very cold and extreme cold stress.");
+            return ("Temperature: " + format + " °C <br> Classed as: Hyper-glacial. <br> Advice: Thermal very cold and extreme cold stress.");
         }
         else if(temperature>=-39&&temperature<=-20){
-            return ("Temperature: " + valor + " °C <br> Classed as: Glacial. <br> Advice: Thermal sensitivity cold and strong cold stress.");
+            return ("Temperature: " + format + " °C <br> Classed as: Glacial. <br> Advice: Thermal sensitivity cold and strong cold stress.");
         }
         else if(temperature>=-19&&temperature<=-10){
-            return ("Temperature: " + valor + " °C <br> Classed as: Extremely cold. <br> Advice: Thermal sensitivity cool and moderate cold stress.");
+            return ("Temperature: " + format + " °C <br> Classed as: Extremely cold. <br> Advice: Thermal sensitivity cool and moderate cold stress.");
         }
         else if(temperature>=-9&&temperature<=-1){
-            return ("Temperature: " + valor + " °C <br> Classed as: Very cold. <br> Advice: Thermal sensitivity slightly cool and slight cold stress.");
+            return ("Temperature: " + format + " °C <br> Classed as: Very cold. <br> Advice: Thermal sensitivity slightly cool and slight cold stress.");
         }
         else if(temperature>=0&&temperature<=13){
-            return ("Temperature: " + valor + " °C <br> Classed as: Cold. <br> Advice: Thermal sensitivity comfortable and no thermal stress.");
+            return ("Temperature: " + format + " °C <br> Classed as: Cold. <br> Advice: Thermal sensitivity comfortable and no thermal stress.");
         }
         else if(temperature>=27&&temperature<=32){
-            return ("Temperature: " + valor + " °C <br> Classed as: Caution. <br> Advice: Fatigue is possible with prolonged exposure and activity. Continuing activity could result in heat cramps.");
+            return ("Temperature: " + format + " °C <br> Classed as: Caution. <br> Advice: Fatigue is possible with prolonged exposure and activity. Continuing activity could result in heat cramps.");
         }
         else if(temperature>=33&&temperature<=41){
-            return ("Temperature: " + valor + " °C <br> Classed as: Alert. <br> Advice: Heat cramps and heat exhaustion are possible. Continuing activity could result in heat stroke.");
+            return ("Temperature: " + format + " °C <br> Classed as: Alert. <br> Advice: Heat cramps and heat exhaustion are possible. Continuing activity could result in heat stroke.");
         }
         else if(temperature>=42&&temperature<=54){
-            return ("Temperature: " + valor + " °C <br> Classed as: Warning. <br> Advice: Heat cramps and heat exhaustion are likely; heat stroke is probable with continued activity.");
+            return ("Temperature: " + format + " °C <br> Classed as: Warning. <br> Advice: Heat cramps and heat exhaustion are likely; heat stroke is probable with continued activity.");
         }
         else if(temperature>54){
-            return("Temperature: " + valor + " °C <br> Classed as: Danger. <br> Advice: Extreme danger, heat stroke is imminent.");
+            return("Temperature: " + format + " °C <br> Classed as: Danger. <br> Advice: Extreme danger, heat stroke is imminent.");
         }
-          return ("Temperature: " + valor + " °C");  
+          return ("Temperature: " + format + " °C");  
     }
     
     public static String seteventObservedTemperature(String valor){
@@ -64,16 +68,17 @@ public class AlertCatalog {
     
     public static String setAlertHumidity(String valor){
         Float humidity = Float.parseFloat(valor);
+        String format = decimalformat.format(humidity);
         if(humidity<=29){
-            return("Relative humidity: "+ valor + " %. <br> Advice: No discomfort.");
+            return("Relative humidity: "+ format + " %. <br> Advice: No discomfort.");
         }else if(humidity>=30&&humidity<=39){
-            return("Relative humidity: "+ valor + " %. <br> Advice: Some discomfort.");
+            return("Relative humidity: "+ format + " %. <br> Advice: Some discomfort.");
         }else if(humidity>=40&&humidity<=45){
-            return("Relative humidity: "+ valor + " %. <br> Advice: Great discomfort: avoid exertion.");
+            return("Relative humidity: "+ format + " %. <br> Advice: Great discomfort: avoid exertion.");
         }else if(humidity>=46&&humidity<=54){
-            return("Relative humidity: "+ valor + " %. <br> Advice: Dangerous.");
+            return("Relative humidity: "+ format + " %. <br> Advice: Dangerous.");
         }else if(humidity>54){
-            return("Relative humidity: "+ valor + " %. <br> Advice: Heat stroke imminent.");
+            return("Relative humidity: "+ format + " %. <br> Advice: Heat stroke imminent.");
         }
         
         return ("Relative humidity: " + valor);
@@ -153,11 +158,10 @@ public class AlertCatalog {
     }
         
     /*public static void main(String [] args){
-            AlertCatalog prueba = new AlertCatalog();
-            System.out.println(prueba);
-            System.out.println(prueba.setAlertTemperature("-40"));
+            DecimalFormat decimalformat = new DecimalFormat("##.##");
+            System.out.println(decimalformat.format(10.232323232323232));
 
-    }*/   
+    }   */
    
 }
 
